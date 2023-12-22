6524056ed1f7fa9b9e3bce7c9b26a38be4e94240
Igor Sobral <igorgmz1421@gmail.com>
2023-12-21 21:42:04 -0300

    ..

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const form = document.querySelector("#form");
 const numeros = [
   1, 2, 3, 4, 5, 2, 3, 6, 7, 8, 1, 2, 3, 4, 5, 6, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-  10, 11, 12,
+  10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+  20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+  39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+  58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76,
+  77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+  96, 97, 98, 99,
 ];
 
 form.addEventListener("submit", function (event) {
@@ -15,7 +20,7 @@ form.addEventListener("submit", function (event) {
   const numbers = numbersValue.value;
   const sequencias = sequenciasValue.value;
 
-  if (!numbers || numbers < 6) {
+  if (!numbers || numbers < 6 || numbers > 20) {
     return;
   }
   if (!sequencias || sequencias < 1) {
@@ -33,23 +38,17 @@ form.addEventListener("submit", function (event) {
 
   for (let index = 0; index < sequencias; index++) {
     const value = contarRepeticoes(numeros, numbers);
+    console.log("🚀 ~ file: script.js:36 ~ value:", value);
     result.push(value);
     const tr = document.createElement("tr");
     const td = document.createElement("td");
     td.innerHTML = `${index + 1}: ${value}`;
     tr.appendChild(td);
     tblbody.appendChild(tr);
-
-    console.log(
-      "🚀 ~ file: script.js:26 ~ contarRepeticoes(numeros, numbers):",
-      contarRepeticoes(numeros, numbers)
-    );
   }
   table.appendChild(tblbody);
   table.classList.add("table-numbers");
   resultElement.appendChild(table);
-
-  console.log("🚀 ~ file: script.js:24 ~ result:", result);
 });
 
 function contarRepeticoes(numeros, numbers) {
@@ -67,14 +66,18 @@ function contarRepeticoes(numeros, numbers) {
 
   // Obter os 12 números mais repetidos
   const numerosMaisRepetidos = listaOrdenada
-    .slice(0, 12)
+    .slice(0, numbers)
     .map((entry) => entry[0]);
 
   // Gerar 6 números aleatórios baseados nos 12 mais repetidos
   const numerosAleatorios = [];
   const numerosDisponiveis = [...numerosMaisRepetidos];
+  console.log(
+    "🚀 ~ file: script.js:71 ~ contarRepeticoes ~ numerosMaisRepetidos:",
+    numerosMaisRepetidos
+  );
 
-  for (let i = 0; i < Math.min(6, numerosMaisRepetidos.length); i++) {
+  for (let i = 0; i < Math.min(numbers, numerosMaisRepetidos.length); i++) {
     const indiceAleatorio = Math.floor(
       Math.random() * numerosDisponiveis.length
     );
